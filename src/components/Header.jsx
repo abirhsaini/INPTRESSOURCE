99fcf91ec89fd6fe7ec5669dcac6396b40a4bf8a
abirhsaini <abirhsaini@gmail.com>
2022-11-24 19:30:41 +0000

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,7 @@ const Header = () => {
                 <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
                 <button type="button" class="btn btn-outline-primary">search</button>
             </div> 
+            <h1></h1>
 
         </div>
     );
